00a0efd18948c328457e851cd6cde01d1878ded8
fs-keri <kritenour@fullsail.edu>
2015-05-12 09:12:19 -0400

    Defining Variables Screencast
    JavaScript file with notes about declaring and defining variables.

--- Variables - Defining/js/script.js
+++ Variables - Defining/js/script.js
@@ -1,1 +1,25 @@
-alert("This is in the js file");
+/*
+ Created by Keri Ritenour
+ Section #1
+ 5/12/15 Week2
+ Defining Variables
+ */
+
+var age; //declaration
+age = 38; //definition
+
+/*
+ Variables are like boxes and declaring a variable is making a box.
+ Declaring a variable is storing something inside that box.
+
+ storage container = value  (the storage container is always on the left
+ and the value is always on the right)
+ */
+
+var yearOfBirth = 1970; //declaring and defining on same line
+
+/*
+ declare your variable before you define it
+ as a rule a number is anything that can be calculated or used
+ to be calculated in the future.
+ */
