cec55244c5bcfcfabd4e51e56573fe633ed447a4
fs-keri <kritenour@fullsail.edu>
2015-05-12 23:21:36 -0400

    Concatenating Strings
    Added JavaScript example for concatenating strings.

---
+++ Expressions - Concatenation/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Keri Ritenour
+ * Section 1
+ * 5/12/15
+ * Expressions - Concatenation
+ */
+
+//var firstName = "Keri";
+//var lasName = "Ritenour";
+//var fullName = firstName + " " + lasName;
+
+//console.log(fullName);
+
+var a = "6"; //if you remove the quotations from both a and b you will get the number 13 diff between strings and nums
+var b = "7";
+
+var result = a + b
+
+console.log(result);
