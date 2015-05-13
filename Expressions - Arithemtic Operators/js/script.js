3f9d4b96cb25630f08239d3009417c1d329cfe3e
fs-keri <kritenour@fullsail.edu>
2015-05-12 22:29:46 -0400

    Arithmetic Operators
    Wrote JavaScript to find the area of a triangle.

--- Expressions - Arithemtic Operators/js/script.js
+++ Expressions - Arithemtic Operators/js/script.js
@@ -5,4 +5,10 @@
  * Expressions - Arithemtic Operators
  */
 
+// area of triangle is half of the width times the height
 
+var width = 4;
+var height = 5;
+var area = width * height/2;
+
+console.log(area);
