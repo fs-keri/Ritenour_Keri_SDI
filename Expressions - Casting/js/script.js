ac7962c83c5a81a2afc0b49de46f054ba956a673
fs-keri <kritenour@fullsail.edu>
2015-05-12 23:33:52 -0400

    Casting
    How to tell the computer to treat numbers and strings and vice versa.

--- Expressions - Casting/js/script.js
+++ Expressions - Casting/js/script.js
@@ -6,3 +6,13 @@
  */
 
 
+//var stringVar = "6";
+//var result = 7 + Number(stringVar);
+//console.log(result);
+
+var areaCode = 540;
+var firstPart = 624;
+var secPart = 8803;
+
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" +String(secPart);
+console.log(phoneNo);
