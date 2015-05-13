4ce834069757d52cc2535c5c1fed05258fac713b
fs-keri <kritenour@fullsail.edu>
2015-05-12 21:09:21 -0400

    String and Boolean Variables

--- Variables - Defining/js/script.js
+++ Variables - Defining/js/script.js
@@ -2,27 +2,15 @@
  Created by Keri Ritenour
  Section #1
  5/12/15 Week2
- Defining Variables
+ String and Boolean Variables
  */
 
-var age; //declaration
-age = 38; //definition
 
+var isStudent = true;
 
+var firstName = "Keri";
 
-/*
- Variables are like boxes and declaring a variable is making a box.
- Declaring a variable is storing something inside that box.
+var phrase = 'I don\'t know';
 
- storage container = value  (the storage container is always on the left
- and the value is always on the right)
- */
-
-var yearOfBirth = 1970; //declaring and defining on same line
-
-/*
- declare your variable before you define it
- as a rule a number is anything that can be calculated or used
- to be calculated in the future.
- */
-
+var phrase = 'I don\'t know \n You\'ll have to speak to the Professor';
+ 
