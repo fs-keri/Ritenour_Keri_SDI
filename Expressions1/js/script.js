f6fccb3a786064f37e159463c7e64d97dd98d43d
fs-keri <kritenour@fullsail.edu>
2015-05-12 22:00:43 -0400

    Basic Expressions Screencast
    Calculating and changing the values of variables.

--- Expressions1/js/script.js
+++ Expressions1/js/script.js
@@ -4,3 +4,16 @@
  * 5/12/15
  * Expressions
  */
+
+//var a =2; //sets up variable and defines with value of 2
+//a + 3; //this appears to add 3 to a
+//console.log(a);
+
+//var a=2;
+//a = a + 3;
+//console.log(a);
+
+var a=2;
+var b;
+b = a + 3;
+console.log(b);
