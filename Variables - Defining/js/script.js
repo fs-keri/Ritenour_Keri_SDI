2e802a8222e52e4087d284324a50c02a196df495
fs-keri <kritenour@fullsail.edu>
2015-05-12 20:32:59 -0400

    String and Boolean Variables
    How to use string and boolean variables. Added this script with notes
    to JavaScript file.

--- Variables - Defining/js/script.js
+++ Variables - Defining/js/script.js
@@ -8,6 +8,8 @@
 var age; //declaration
 age = 38; //definition
 
+
+
 /*
  Variables are like boxes and declaring a variable is making a box.
  Declaring a variable is storing something inside that box.
@@ -23,3 +25,19 @@ var yearOfBirth = 1970; //declaring and defining on same line
  as a rule a number is anything that can be calculated or used
  to be calculated in the future.
  */
+
+/*Boolean variable - they are the simplest and smallest pieces of data
+  They are either on or off they store either true or false
+ */
+
+var isStudent = true;
+
+var firstName = "Keri"; //you can use single or double quotes as long as your are consistant
+
+var phrase = 'I don\'t know'; /* in this instance you can use the escape operator that tells the machine
+ to ignore certain characters so here you would use the backslash
+  */
+
+var phrase = 'I don\'t know \n You\'ll have to speak to the Professor';/*the backslash n creates a new line
+  or line break
+  */
