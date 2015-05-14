0f6f83afb3f066b1b242c2043436df1617e3a11d
fs-keri <kritenour@fullsail.edu>
2015-05-13 23:49:03 -0400

    Calculating Dog Actual age from human age
    Calculation that takes a dogs human years and multiplies it by 7 to get
    the dogs actual age.

--- Expressions_worksheet/Dog Years/js/script.js
+++ Expressions_worksheet/Dog Years/js/script.js
@@ -5,4 +5,13 @@
  * Activity: Expression Worksheet - Dog Years
  */
 
-alert("Testing to see if connected.");
+//alert("Testing to see if connected.");
+
+//Calculating dog years based on actual years
+var dogAge=3;
+const dogYears=7;
+
+//multiply the the dog human age times to get the dogs actual age
+var result= dogAge * dogYears
+console.log(result);
+
