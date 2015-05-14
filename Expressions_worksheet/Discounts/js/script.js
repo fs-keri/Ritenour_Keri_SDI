7f298600e3c58cc5425b1d8aae06aae2fb0b83ba
fs-keri <kritenour@fullsail.edu>
2015-05-14 19:25:40 -0400

    Total of item minus discount
    This works, now I need to add in the other results.

--- Expressions_worksheet/Discounts/js/script.js
+++ Expressions_worksheet/Discounts/js/script.js
@@ -9,12 +9,18 @@
 
 //Calculate the discounted price for an item with and without sales tax
 var itemPrice= 40.00;
-var discount= 20;
+var discount= .20;
 var itemDescription= "perfume";
 var taxPercentage= .065;
 
 
 //multiply the item price by the discount percentage
-var discountPercentage= (discount / 100);
-//var discountedPrice = (itemPrice - (100 * discount));
-console.log(discountPercentage);
+//var discountPercentage= (discount / 100);
+
+//Testing to see if percentage calculation works
+//console.log(discountPercentage);
+
+var totalDiscount= itemPrice - (itemPrice * discount);
+
+//var totalwithTax = (totalDiscount * taxPercentage);
+console.log(totalDiscount);
