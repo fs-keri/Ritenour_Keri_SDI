f1e7c609ab4bf27ec7fd3251efd187abb2a40c2b
fs-keri <kritenour@fullsail.edu>
2015-05-14 00:15:01 -0400

    Result of Slice or Pie Part 1 and Part 2
    Calculations using division and modulo

--- Expressions_worksheet/Slice of Pie Part 1/js/script.js
+++ Expressions_worksheet/Slice of Pie Part 1/js/script.js
@@ -5,4 +5,14 @@
  * Activity: Expression Worksheet - Slice of Pie Part 1
  */
 
-alert("Testing to see if connected.");
+//alert("Testing to see if connected.");
+
+//Calculate the number of slices of pizza each person will receive
+var pizzaSlice=50;
+var people=16;
+
+//divide the number of slices of pizza by the number of people
+var result= pizzaSlice / people;
+
+//print out the number of slices each person will get
+console.log("Each person ate " +result+ " slices of pizza at the party");
