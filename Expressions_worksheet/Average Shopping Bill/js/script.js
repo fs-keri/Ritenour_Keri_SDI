2fc66abbe499af3b8e2c9674ffacb5a8eec211a0
fs-keri <kritenour@fullsail.edu>
2015-05-14 00:28:50 -0400

    Calculated total and average
    Calculated total and average amount of groceries spent in the past 5
    weeks using addition and division.

--- Expressions_worksheet/Average Shopping Bill/js/script.js
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -5,4 +5,14 @@
  * Activity: Expression Worksheet - Average Shopping Bill
  */
 
-alert("Testing to see if connected.");
+//alert("Testing to see if connected.");
+
+//Calculate the average spending amount for groceries over the past 5 weeks
+var groceryTotals =[249.25, 141.56, 121.03, 142.27, 92.89];
+
+//Calculate the total amount spent on groceries
+var total= groceryTotals[0] + groceryTotals[1] + groceryTotals[2]+ groceryTotals[3] + groceryTotals[4];
+
+//Calculate the average weekly total spending on groceries
+var average = total /5;
+console.log("You have spent a total of $" +total+ " on groceries over 5 weeks. That is an average of $" +average+ " per week");
