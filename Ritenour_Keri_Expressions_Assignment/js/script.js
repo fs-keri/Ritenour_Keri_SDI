0ce09661ab6e597ddf6f2d700d2cf56703ed12ba
fs-keri <kritenour@fullsail.edu>
2015-05-14 22:55:48 -0400

    Final Testing and of Values
    Completed final testing of values and added multi-lined comment of
    testing that was performed.

--- Ritenour_Keri_Expressions_Assignment/js/script.js
+++ Ritenour_Keri_Expressions_Assignment/js/script.js
@@ -25,10 +25,22 @@ var supportPrompt = prompt("Which support package would you like to purchase?");
 console.log(supportPrompt);
 
 //calculate the package total based on user prompted arrays
-var packTotal = (webPack[2] + hostPack[0] + supportPack[2]);
+var packTotal = (webPack[3] + hostPack[3] + supportPack[2]);
 console.log(packTotal);
 
 //calculate the monthly payments based on the package total
 var monthlyPay = packTotal / 12;
-console.log("The website package you selected is $" +webPack[2]+ ", the hosting package is $" +hostPack[0]+ " and the support " +
+console.log("The website package you selected is $" +webPack[3]+ ", the hosting package is $" +hostPack[3]+ " and the support " +
     "package is $" +supportPack[2]+ ". Your total package price is $" +packTotal+ ". Your monthly payments for one year are $" +monthlyPay+ ".");
+
+
+/*Final Testing
+    I typed in 2 for webPack, 0 for hostPack and 2 for supportPack.
+    It gave me a total of $1000 and a monthly payment of $83.33333333333333.
+
+    I typed in 0 for webPack, 3 for hostPack and 2 for supportPack.
+    It gave me a total of $700 and a monthly payment of $58.333333333333336.
+
+    I typed in 3 for webPack, 3 for hostPack and 2 for supportPack.
+    It gave me a total of $1300 and a monthly payment of $108.33333333333333.
+ */
