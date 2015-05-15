60a8820c9e2250deeb6db1d8e939440fbb60bc5a
fs-keri <kritenour@fullsail.edu>
2015-05-14 21:34:00 -0400

    Created Array and Prompt
    Created js array for website package prompt.

--- Ritenour_Keri_Expressions_Assignment/js/script.js
+++ Ritenour_Keri_Expressions_Assignment/js/script.js
@@ -5,4 +5,11 @@
  * Expressions Assignment
  */
 
-alert("Testing to see if connected.");
+//alert("Testing to see if connected.");
+
+//Calculator for monthly payments of one year website, hosting, and support packages
+
+//create array for the website package
+var webPack = [200, 400, 600, 800];
+var webPrompt = prompt("Which website package would you like to purchase?");
+console.log(webPrompt);
