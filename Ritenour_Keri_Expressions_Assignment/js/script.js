57c83160c93f2a221726748839ccd2ad80fc6530
fs-keri <kritenour@fullsail.edu>
2015-05-14 22:40:08 -0400

    Final Code for Assignment
    Removed prior alerts that were not needed and cleanup and testing of
    final code. Ensured use of 2 arithmetic operators, an array, a string
    variable for output and at least 1 assignment operator. Ensured
    commenting and meaningful output.

--- Ritenour_Keri_Expressions_Assignment/js/script.js
+++ Ritenour_Keri_Expressions_Assignment/js/script.js
@@ -14,24 +14,15 @@ var webPack = [200, 400, 600, 800];
 var webPrompt = prompt("Which website package would you like to purchase?");
 console.log(webPrompt);
 
-//alert the user of the package they have selected
-//alert("You have selected website package number " +webPrompt+ " .");
-
 //create array for the hosting package
 var hostPack = [100, 125, 175, 200];
 var hostPrompt = prompt("Which hosting package would you like to purchase?");
 console.log(hostPrompt);
 
-//alert the user of the hosting package they have selected
-//alert("You have selected hosting package number " +hostPrompt+ " .");
-
 //create the support package
 var supportPack = [100, 200, 300];
 var supportPrompt = prompt("Which support package would you like to purchase?");
 console.log(supportPrompt);
-
-//alert the user of the support package they have selected
-//alert("You have selected support package number " +supportPrompt+ " .");
 
 //calculate the package total based on user prompted arrays
 var packTotal = (webPack[2] + hostPack[0] + supportPack[2]);
@@ -39,5 +30,5 @@ console.log(packTotal);
 
 //calculate the monthly payments based on the package total
 var monthlyPay = packTotal / 12;
-console.log("The website package you selected is $" +webPack[2]+ " , the hosting package is $" +hostPack[0]+ " and the support " +
+console.log("The website package you selected is $" +webPack[2]+ ", the hosting package is $" +hostPack[0]+ " and the support " +
     "package is $" +supportPack[2]+ ". Your total package price is $" +packTotal+ ". Your monthly payments for one year are $" +monthlyPay+ ".");
