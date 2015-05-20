1d2f07c9307f46216ba952e7cd1f0a2d4630fa3d
fs-keri <kritenour@fullsail.edu>
2015-05-19 23:00:05 -0400

    Conditionals Simple Example
    Simple example of conditionals using if statement.

--- Week 3 [Conditionals]/js/script.js
+++ Week 3 [Conditionals]/js/script.js
@@ -1,3 +1,14 @@
 /**
  * Created by kerilritenour on 5/19/15.
  */
+
+
+//Conditional Logic
+
+var oldEnough=true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
