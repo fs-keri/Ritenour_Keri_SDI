ba8d71cae26a29000bc97a8f81cf453952428089
fs-keri <kritenour@fullsail.edu>
2015-05-20 19:24:07 -0400

    Created array
    created bowl of fruit array and counting variable to track pears.

--- GoTo Training Week3/js/script.js
+++ GoTo Training Week3/js/script.js
@@ -7,3 +7,22 @@
 
 //alert("Testing to see if connected");
 
+//create a calculator that keeps track of how many pears are in a fruit bowl
+
+//Create our virtual fruit bowl as an array
+var bowlOfFruit =["apple", "banana", "pear", "peach"];
+
+//print our the whole array
+console.log(bowlOfFruit);
+
+//to print out a specific fruit, use the index number
+//index numbers start with 0, not 1
+console.log(bowlOfFruit[0])
+
+//how many items are inside of the array
+//property of an array called length
+//Dot Syntax - fancy for use a period
+console.log(bowlOfFruit.length);
+
+//create a variable to count the number of pears
+var totalNumPears = 0;
