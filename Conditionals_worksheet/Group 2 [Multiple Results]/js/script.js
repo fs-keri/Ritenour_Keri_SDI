00f98daa355c135e3366969c8dc7f4fd216c66c1
fs-keri <kritenour@fullsail.edu>
2015-05-21 09:12:05 -0400

    Updates to Group 3

--- Conditionals_worksheet/Group 2 [Multiple Results]/js/script.js
+++
@@ -1,8 +0,0 @@
-/**
- * Keri Ritenour
- * 5/20/15
- * Section 1
- * Conditionals Worksheet - Group 2 - Multiple Results
- */
-
-//alert("Testing to see if connected.");
--- Conditionals_worksheet/Group 3 [Multiple Conditions]/js/script.js
+++ Conditionals_worksheet/Group 3 [Multiple Conditions]/js/script.js
@@ -6,3 +6,14 @@
  */
 
 //alert("Testing to see if connected.");
+
+//The local movie theater in town has a ticket price of $12, but if you are a senior (55 or older) or under 10 you get
+// a discounted price of $7. In addition, if a customer is seeing a movie between 3pm and 5pm, they can also get
+// the discounted price.
+
+//Given
+// Time of movie (assume whole numbers here)
+// Age of the customer
+
+//Results to Print Out
+// "The ticket price is X"
