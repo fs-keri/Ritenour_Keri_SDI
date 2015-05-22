74783f0e792595fd0889eb0a2f598aec73b10a97
fs-keri <kritenour@fullsail.edu>
2015-05-21 23:04:40 -0400

    Calculator to determine total cost of car and monthly payments
    Created variable for user to input the cost of the vehicle. If blank
    the user is prompted again to enter the cost of the car.

--- Ritenour_Keri_Conditionals_Assignment/js/script.js
+++ Ritenour_Keri_Conditionals_Assignment/js/script.js
@@ -7,3 +7,14 @@
 
 //alert("Testing to see if connected");
 
+//Create a calculator to determine the total cost of a vehicle and the monthly payments
+
+//create a variable to prompt the user for the cost of the vehicle
+var vehPrice=prompt("What is the cost of the vehicle");
+//validate for both blank and a number
+if(vehPrice==="" && isNaN){
+    vehPrice=prompt("You must enter the cost of the vehicle.");
+}else {
+    var vehPrice;
+}
+console.log(vehPrice);
