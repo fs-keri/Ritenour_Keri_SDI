c4083a952011d73bf68f48eeef895bc17126a664
fs-keri <kritenour@fullsail.edu>
2015-05-21 23:11:49 -0400

    Months of loan
    Created a variable that prompts the user for the months they would like
    the loan. If the user enters less than 24 months, they are prompted to
    enter a number again.

--- Ritenour_Keri_Conditionals_Assignment/js/script.js
+++ Ritenour_Keri_Conditionals_Assignment/js/script.js
@@ -22,9 +22,19 @@ console.log(vehPrice);
 //create a variable to prompt the user for the interest rate they would like
 var interestRate=prompt("Select either a 6, 8 or 9 percent interest rate?");
 //validate for both blank and a number
-if(Number(interestRate==="" || isNaN)){
+if (interestRate==="" || isNaN){
     interestRate=prompt("Please do not leave blank and enter the interest rate");
 }else {
     var interestCompute=interestRate/100;
 }
 console.log(interestCompute);
+
+//create a variable to prompt the user for the months they would like to finance the vehicle
+var monthsPrompt=prompt("How many months would you like to finance the vehicle?");
+//validate for both blank and less than 24 months
+if(monthsFinanced < 24){
+    monthsFinanced=prompt("We only give loans for longer than 24 months. Please enter an amount greater than 24 months");
+}else{
+    var monthsFinanced = monthsPrompt;
+}
+console.log(monthsFinanced);
