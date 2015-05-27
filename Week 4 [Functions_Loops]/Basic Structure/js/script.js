c5fca98cf1e9db59b6909572f81ecdbfeeba3868
fs-keri <kritenour@fullsail.edu>
2015-05-26 20:10:36 -0400

    Updated Notes and Function Execution
    made updates to notes and created new file structure for function
    execution.

--- Week 4 [Functions_Loops]/Basic Structure/js/script.js
+++ Week 4 [Functions_Loops]/Basic Structure/js/script.js
@@ -4,3 +4,14 @@
  * Section 1
  * Functions - Basic Structure
  */
+
+function outptMsg(){
+    console.log("Hello Wordl");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
