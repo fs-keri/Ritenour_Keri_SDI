db933ca5d36a1ad35f5f0d47482f06e98a87b704
fs-keri <kritenour@fullsail.edu>
2015-05-26 20:57:58 -0400

    Updated Notes and Created Returning Values Files and Script

---
+++ Week 4 [Functions_Loops]/Function_Return Values/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Keri Ritenour
+ * 5/26/15
+ * Section 1
+ * Functions - Returning Values
+ */
+
+var total = calcArea(30, 20); //whatever this returns store it in the variable
+
+function calcArea(w, h){
+    var area = w * h;
+    return area; //is being returned to calcArea
+}
+
+console.log(total);
