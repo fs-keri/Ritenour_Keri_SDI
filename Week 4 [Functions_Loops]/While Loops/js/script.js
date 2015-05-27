1fb97932d52180a5328a71ef252dfbfac478d753
fs-keri <kritenour@fullsail.edu>
2015-05-26 23:11:36 -0400

    Code for While Loop Screencast

--- Week 4 [Functions_Loops]/While Loops/js/script.js
+++ Week 4 [Functions_Loops]/While Loops/js/script.js
@@ -6,3 +6,10 @@
  */
 
 //alert("Testing to see if connected");
+
+var b =50; //sets up the index
+
+while (b > 0){  //this is saying while b>0, do this code IT CHECKS THE CONDITION
+    console.log(b+ " kegs on the wall.");
+    b--; //we need this to stop it or ++ to increase it THIS INCREMENTS OR DECREMENTS THE INDEX
+}
