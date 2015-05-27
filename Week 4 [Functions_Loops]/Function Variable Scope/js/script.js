ba1c7f46c0d99e813d23dc2cfd665df7623e04b0
fs-keri <kritenour@fullsail.edu>
2015-05-26 20:13:17 -0400

    File structure for Variable Scope Screencast
    created file structure for variable scope screencast.

---
+++ Week 4 [Functions_Loops]/Function Variable Scope/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Keri Ritenour
+ * 5/26/15
+ * Section 1
+ * Functions - Variable Scope
+ */
+
+//define the function
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+
+//Call the function
+calcArea();
