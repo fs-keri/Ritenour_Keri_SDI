9b74c534a665e875cad67b0e5880702aa2b76b31
fs-keri <kritenour@fullsail.edu>
2015-05-26 22:46:57 -0400

    Calculate Circumference of a circle

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -7,8 +7,8 @@
 
 //alert("Testing to see if connected");
 
-//Calculate the Area of a Rectangle
 
+//Example: Calculate the Area of a Rectangle
 var width = 5;
 var height = 6;
 var area = calculateArea(width, height);
@@ -17,3 +17,24 @@ console.log("The Area of a Rectangle is " +area);
 function calculateArea(w,h){
     return w*h;
 }
+
+
+
+//Calculate the Circumference of a circle.
+// create a variable for the radius
+var radius = 2;
+
+//create a variable for the result
+var circum = calculateCircum(radius);
+
+//create result to print to console
+console.log("The circumference of the circle is " +circum);
+
+function calculateCircum(radius){
+    return ((radius * 2) * 3.14);
+}
+
+
+
+//Calculate how many bee stings are needed to kill an animal
+//create a variable for the victim's weight
