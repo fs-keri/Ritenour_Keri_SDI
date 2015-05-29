0a32b1d6cc59fec4509df23d32d5a513d8787972
fs-keri <kritenour@fullsail.edu>
2015-05-28 21:44:50 -0400

    User Prompts for weight, feet and inches
    Created user prompts for weight, feet and inches and validation.

--- Ritenour_Keri_Functions_Assignment/js/script.js
+++ Ritenour_Keri_Functions_Assignment/js/script.js
@@ -15,3 +15,46 @@
    all user prompts must be validated using a while loop
  */
 
+//Create a calculator that returns the body mass index (BMI)
+
+//Prompt the user to enter their weight
+var weight = prompt("Please enter your weight in pounds:");
+
+//validate the prompt
+while(weight==="" || isNaN(weight)){
+    //re-prompt the user with the same variable name
+    if(weight===""){
+        weight=prompt("Please do not leave blank. \nEnter your weight in pounds.");
+    }else if(isNaN(weight)){
+        min=prompt("Please only use numbers. \nEnter your weight.");
+    }
+}
+
+
+//prompt the user to enter their height in feet
+var feetHeight = prompt("Now, enter your tall you are in feet:");
+
+//validate the prompt
+while(feetHeight==="" || isNaN(feetHeight)){
+    //re-prompt the user with the same variable name
+    if(feetHeight===""){
+        feetHeight=prompt("Please do not leave blank. \nEnter how tall you are in feet.");
+    }else if(isNaN(feetHeight)){
+        feetHeight=prompt("Please only use numbers. \nEnter your height in feet.");
+    }
+}
+
+//prompt the user to enter their inches in height
+var inchHeight = prompt("Are there any inches associated with your height?");
+
+//validate the prompt
+while(inchHeight==="" || isNaN(inchHeight)){
+    //re-prompt the user with the same variable name
+    if(inchHeight===""){
+        inchHeight=prompt("Please do not leave blank. \nEnter any inches associated with your height.");
+    }else if(isNaN(inchHeight)){
+        inchHeight=prompt("Please only use numbers. \nEnter the inches associated with your height. \nIf 0, then enter 0.");
+    }
+}
+
+//
