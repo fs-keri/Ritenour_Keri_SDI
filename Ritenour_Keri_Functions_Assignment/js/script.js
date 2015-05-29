f0c693aae58fa7977079d4cc443a507a2486327f
fs-keri <kritenour@fullsail.edu>
2015-05-28 21:01:09 -0400

    Starting comments to assignment
    Added comments on instructions for assignment.

--- Ritenour_Keri_Functions_Assignment/js/script.js
+++ Ritenour_Keri_Functions_Assignment/js/script.js
@@ -5,4 +5,13 @@
  * Functions Assignment
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+/* Code that creates a calculator that prompts the user to enter 3 pieces of information and validates that the inputs,
+   uses 2 functions (1 normal, 1 anonymous);
+   uses arguments and parameters, one function must use 3 parameters;
+   a value must be returned for each function and saved in a variable in the main code;
+   main code variable must not be used inside a function - must pass into an argument;
+   all user prompts must be validated using a while loop
+ */
+
