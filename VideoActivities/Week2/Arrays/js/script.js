7c4bb148d13d530828940632f88b291adacfe114
fs-keri <kritenour@fullsail.edu>
2015-06-07 23:57:19 -0400

    Arrays Screencast
    Created js and html files for Arrays Screencast.

---
+++ VideoActivities/Week2/Arrays/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Arrays
+ */
+
+//Arrays are good for collections or lists
+
+var muppetNames =["Kermit", "Miss Piggy", "Gonzo", "Rizzo" ];
+//arrays are not restricted to one type, they can hold strings, numbers, etc that are mixed
+
+/*Each value has an index number, starting with 0
+    Kermit is 0
+    Miss Piggy is 1
+    Gonzo is 2
+    Rizzo is 3
+    */
+
+//console.log(muppetNames);
+
+//To get to one of these arrays, we need to use Array Access Notation
+//console.log(muppetNames[2]);
+
+muppetNames[1]= "Rizzo"; //sets value of 1 from Miss Piggy to Rizzo
+muppetNames[2] = "Fozzy";
+console.log(muppetNames[2]);
+
