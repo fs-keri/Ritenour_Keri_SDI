059b7763ed3049b8d52c0812eec00c67b15b3078
fs-keri <kritenour@fullsail.edu>
2015-06-07 23:21:35 -0400

    Defining Variables Screencast
    Setup js and html files for Defining Variables screencast and followed
    along.

---
+++ VideoActivities/Week2/Defining Variables/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Defining Variables
+ */
+
+var age; //declaring variable
+age = 38; //defining variable
+
+/*
+    Remember that variables are like boxes
+    Declaring a variable is making a box
+    Defining a variable is storing something inside that box
+    Storage container always on left = value always on right
+ */
+
+var yearOfBirth = 1970;
+//declared on left = defined on right
+
+/*
+    As a rule, a number should be anything that can be calculated
+    or used as a calculation in the future.
+ */
