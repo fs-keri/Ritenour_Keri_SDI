d586b8f2be0918f068767a1856405666a0275e32
fs-keri <kritenour@fullsail.edu>
2015-06-07 23:09:26 -0400

    Values and Variable Screencast
    Created js and html file and followed along screencast. Includes rules
    on naming variables and conventions.

---
+++ VideoActivities/Week2/Values_Variables/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Declaring Variables
+ */
+
+var age;
+
+/* Rules for naming variables
+        Avoid special characters
+        Use only letters, numbers and/or underscores
+        Do not begin with numbers
+
+   Conventions
+        Names should be relevant and meaningful
+        Use "camel case" aka camelCase
+        Begin with lower case letters
+ */
