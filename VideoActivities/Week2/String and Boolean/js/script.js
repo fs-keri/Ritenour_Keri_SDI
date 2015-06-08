0cd9c07e13ef16370282292e1bbe8ae7c66e7d3a
fs-keri <kritenour@fullsail.edu>
2015-06-07 23:33:17 -0400

    String and Boolean Screencast
    Created js and html files for String and Boolean screencast.

---
+++ VideoActivities/Week2/String and Boolean/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Variables of Different Types, Numbers, Booleans and Strings
+ */
+
+var age= 38; //Number variable
+
+var isStudent = false; //Boolean variable
+
+var firstName ="Keri"; //String variable
+
+var phrase = 'I don\'t know'; //use of escaping character tells computer to ignore second quotation mark
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the Professor';
+//escaping character with an n as \n starts a new line
