b60ad91ff7d6f4b04387091a78357f8f957453bd
fs-keri <kritenour@fullsail.edu>
2015-06-09 00:35:44 -0400

    Week 2 Concatenating Strings Screencast
    Created html and js files for Concatenating Strings screencast and
    followed along.

---
+++ VideoActivities/Week2/Concatenating Strings/js/script.js
@@ -0,0 +1,21 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Concatenating Strings
+ */
+
+/*
+var firstName = "Keri";
+var lastName = "Ritenour";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+ */
+
+var a ="6";
+var b ="7";
+
+var results = a + b; //this takes the 2 strings and combines them, not add them
+console.log(results);
+
