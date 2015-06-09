8a881ad1ad967336138b1ac64f9da5a7c26bcaf2
fs-keri <kritenour@fullsail.edu>
2015-06-09 00:28:14 -0400

    Completed Week 2 Arrays in Expressions Screencast
    Created js and html file for Arrays in Expressions Screencast and
    followed along.

---
+++ VideoActivities/Week2/Arrays in Expressions/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Arrays in Expressions
+ */
+
+var orangeBins =[234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
