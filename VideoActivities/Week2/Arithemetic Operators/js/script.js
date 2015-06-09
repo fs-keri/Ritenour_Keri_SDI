d36b63ea6d14d6874ceb85a6ba6ec33ce67389f5
fs-keri <kritenour@fullsail.edu>
2015-06-09 00:02:36 -0400

    Week 2 Arithmetic Operators Screencast
    Followed along and completed code for calculating the area of a
    triangle.

--- VideoActivities/Week2/Arithemetic Operators/js/script.js
+++ VideoActivities/Week2/Arithemetic Operators/js/script.js
@@ -4,3 +4,9 @@
  * Section 02
  * Week 2 - Arithmetic Operators
  */
+
+//area of the triangle is half of the width times the height
+var width = 4;
+var height= 5;
+var area = width * height /2;
+console.log(area);
