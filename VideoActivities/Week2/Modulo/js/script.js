ddcf7120d1985d4318ca693bb86cdbeacace081a
fs-keri <kritenour@fullsail.edu>
2015-06-09 00:13:01 -0400

    Completed Modulo Screencast
    Followed along and completed.

---
+++ VideoActivities/Week2/Modulo/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Modulo
+ */
+
+//the modulo operator finds the remainder of a number only - it uses the % sign
+
+var remainder = 33 % 2;
+console.log(remainder);
+
+/*great to use to see how numbers are sorted or if you are trying to find out is a number is even or odd
+ Even numbers divided by 2 result in a modulo of 0
+ Odd numbers divided by 2 result in a modulo of 1
+ */
