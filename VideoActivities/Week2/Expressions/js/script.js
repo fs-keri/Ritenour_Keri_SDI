29495d83b0ee37eaaeae16ccb9c0d9a228d1b446
fs-keri <kritenour@fullsail.edu>
2015-06-08 23:44:20 -0400

    Completed Expressions Screencast
    Followed along and completed js code for basic expressions.

--- VideoActivities/Week2/Expressions/js/script.js
+++ VideoActivities/Week2/Expressions/js/script.js
@@ -5,4 +5,19 @@
  * Week 2 - Expressions
  */
 
-alert("Testing to see if connected");
+var a = 2; //sets up our variable a and defines it with 2
+a + 3; //add 3 to a
+
+console.log(a);
+//notice that this does not change a, because there is no where to store the value to do so you need an assignment operator
+
+a = a + 3; //this will take the value of a and add it to 3 and store it in a
+
+/*Remember in your expressions to include an assignment operator in order
+to store the result of the expression
+*/
+
+
+//it could be that you store it in another
+var b;
+b= a + 3;
