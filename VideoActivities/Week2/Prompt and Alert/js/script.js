f5383fb6ae3f0c280357ec02cd5248dabc3530c6
fs-keri <kritenour@fullsail.edu>
2015-06-09 18:19:08 -0400

    Week 2 Prompt and Alert Screencast.
    Completed and followed along with Prompt and Alert screencast,
    calculating area of rectangle and prompting and alerting.

--- VideoActivities/Week2/Prompt and Alert/js/script.js
+++ VideoActivities/Week2/Prompt and Alert/js/script.js
@@ -5,4 +5,19 @@
  * Week 2 - Prompt and Alert
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+prompt("Enter your year of birth:");  //not stored anywhere so to capture it we need to assign it to a variable
+
+var userInput = prompt("Enter your year of birth:"); //prompts user and stores it
+console.log(userInput); //prints so you can see it  - but user cannot see it
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width:");
+var height = prompt("Please enter the height:");
+//the above lets you enter the information and saves it
+
+var area = width * height; //calculates information from prompt entry
+//console.log(area); //prints it out to the console
+
+var result = ("The area of your rectangle is " + area + " sq feet.");
+alert(result);
