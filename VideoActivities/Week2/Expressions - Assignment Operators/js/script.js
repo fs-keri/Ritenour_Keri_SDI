5469ff3721d7922d30d16c3e44fe6e11ee6ec08b
fs-keri <kritenour@fullsail.edu>
2015-06-09 18:02:46 -0400

    Week 2 Assignment Operators Screencast
    Completed and followed along with screencast.

--- VideoActivities/Week2/Expressions - Assignment Operators/js/script.js
+++ VideoActivities/Week2/Expressions - Assignment Operators/js/script.js
@@ -5,4 +5,35 @@
  * Week 2 - Expressions - Assignment Operators
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+var a = 3;
+a + 4;
+console.log(a); //if you want to do more with and use a then you need to assign a
+
+a = a+4;
+console.log(a);
+
+//add
+a+=4; //this is the same as writing a = a+4
+console.log(a);
+
+//multiply
+a *=4;
+console.log(a);
+
+//divide
+a /=4;
+console.log(a);
+
+//subtract
+a -=4;
+console.log(a);
+
+//increment operator
+
+a ++;
+console.log(a);
+
+a --;
+console.log(a);
