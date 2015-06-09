0b26980e57c2764375d2da2b03d6996f98a139aa
fs-keri <kritenour@fullsail.edu>
2015-06-09 17:51:37 -0400

    Week 2 Casting Screencast
    Followed along and completed casting screencast.

--- VideoActivities/Week2/Casting/js/script.js
+++ VideoActivities/Week2/Casting/js/script.js
@@ -5,4 +5,21 @@
  * Week 2 - Casting
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+var stringVar = "6";
+var result = 7 + Number(stringVar);//this says treat this like a number
+console.log(result);
+
+var areaCode = 407;
+var firstPart = 203;
+var secondPart = 4529;
+var phoneNo = areaCode + firstPart + secondPart;
+console.log (phoneNo); //this will not string these together, they need casted
+
+var phoneNo = String(areaCode) + String(firstPart) + String(secondPart);
+console.log (phoneNo);
+
+//to get it to look like a phone number (407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" +String(secondPart);
+console.log (phoneNo);
