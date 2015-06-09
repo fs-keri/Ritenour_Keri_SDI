5c4dc6e5a29b30e4320f569b857b9e87fa4ea689
fs-keri <kritenour@fullsail.edu>
2015-06-08 23:52:30 -0400

    Week 2 Expressions Screencast
    Created js and html files for calculating age.

---
+++ VideoActivities/Week2/Expressions - Age Example/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Keri Ritenour
+ * 6/8/15
+ * Section 02
+ * Week 2 - Expressions - Age Example
+ */
+
+
+var yearBorn = 1968;
+var age = 2015 - yearBorn;
+//Can't see this until you print it to the console
+console.log(age);
