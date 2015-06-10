25edfd2ef2c3753c52573ef7575d6455dde9006c
fs-keri <kritenour@fullsail.edu>
2015-06-09 23:11:35 -0400

    Expressions Worksheet Slice of Pie Part 1 Problem
    Created js and html files for Slice of Pie Part 1. Tested to see if
    connected.

---
+++ Expressions_Worksheet/Slice of Pie Part 1/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Keri Ritenour
+ * 6/9/15
+ * Section 02
+ * Expressions Worksheet - Slice of Pie Part 1
+ */
+
+alert("Testing to see if connected");
+
+/*
+ A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza each partygoer will get at the party. (Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
+
+ Given:
+    Number of slices per pizza
+    Number of people at the party
+    Number of pizzas ordered.
+
+ Result Variables:
+    Number of slices per person. (Can be a decimal or floating point).
+
+ Result to Print:
+    “Each person ate X slices of pizza at the party.”
+ */
