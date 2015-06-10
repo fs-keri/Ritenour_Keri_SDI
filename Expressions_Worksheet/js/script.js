1957fb2bc1fb95329554d96b5b75607662b9f429
fs-keri <kritenour@fullsail.edu>
2015-06-09 22:45:45 -0400

    Calculate area of a circle
    Entered example of calculating the area of a circle and printed the
    output to the screen.

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -5,4 +5,24 @@
  * Expressions Worksheet
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+/*Problem: Calculate the area of a circle given the radius of a circle.
+    Givens:
+        The radius of the circle.
+        The constant value of Pi.
+    Result Variable:
+        The area of the circle.
+
+    Result to Print:
+    “The circumference of the circle is x”;
+*/
+//Calculate the circumference of a circle;
+var radius = 6;
+const Pi = 3.14159265;
+
+//multiply 2, PI and the radius & assign to circ variable.
+var circ = radius * 2 * Pi;
+
+//print out the output circumference
+console.log("The circumference of the circle is " +circ);
