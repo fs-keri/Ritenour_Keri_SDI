5ca1bf6d5d0a35c98522f9db2f5a88970aec67dc
fs-keri <kritenour@fullsail.edu>
2015-06-09 23:20:38 -0400

    Slice of Pie Part 1
    Created variables for slices of pizza, number of people, and number of
    pizzas ordered. Calculated and printed to console.

--- Expressions_Worksheet/Slice of Pie Part 1/js/script.js
+++ Expressions_Worksheet/Slice of Pie Part 1/js/script.js
@@ -5,7 +5,7 @@
  * Expressions Worksheet - Slice of Pie Part 1
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
 
 /*
  A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza each partygoer will get at the party. (Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
@@ -20,4 +20,22 @@ alert("Testing to see if connected");
 
  Result to Print:
     “Each person ate X slices of pizza at the party.”
+ */
+
+//create variables for slices of pizza, number of people, and number of pizza ordered
+var slicePizza = 10;
+var partyPeople = 20;
+var pizzaOrdered = 5;
+
+//calculation of number of slices per person
+var result = (slicePizza * pizzaOrdered) / partyPeople;
+
+//print to console
+console.log("Each person ate " +result+ " slices of pizza at the party.");
+
+/*
+    Entered 8 slices of pizza, 4 people, and 10 ordered and got 20 slices of pizza
+    Entered 10 slices of pizza, 8 people, and 4 ordered and got 5 slices of pizza
+    Entered 10 slices of pizza, 20 people, and 5 ordered and got 2.5 slices of pizza
+
  */
