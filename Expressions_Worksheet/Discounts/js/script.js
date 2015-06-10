c880310cbccb671a76b708e1bec4479539daaddd
fs-keri <kritenour@fullsail.edu>
2015-06-09 23:46:33 -0400

    Added problem descriptions

--- Expressions_Worksheet/Discounts/js/script.js
+++ Expressions_Worksheet/Discounts/js/script.js
@@ -5,4 +5,22 @@
  * Expressions Worksheet - Discounts
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+/*
+ Calculate the discounted price for an item. Create an expression that will calculate the discounted price with and without sales tax.
+ (It is acceptable for the result to have more than two digits after the decimal. $345.896 for example.)
+
+ Givens:
+    Original price
+    Discount percentage (20% would be 20, for example)
+    Description of item
+    Sales tax percentage
+
+ Result Variables:
+    Price of the item with tax
+    Price of the item without tax
+
+ Result to Print:
+    “Your X was originally $X, but after a X% discount, it is now $X without tax, and $X with tax.”
+ */
