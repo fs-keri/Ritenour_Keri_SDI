c2228aeedb395d266cf7f1f295c8bc1b27559d6c
fs-keri <kritenour@fullsail.edu>
2015-06-09 23:46:13 -0400

    Added problem descriptions

--- Expressions_Worksheet/Average Shopping Bill/js/script.js
+++ Expressions_Worksheet/Average Shopping Bill/js/script.js
@@ -5,4 +5,23 @@
  * Expressions Worksheet - Average Shopping Bill
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+/*
+ You are budgeting your money for the year and must calculate your average weekly grocery shopping
+ spending over the past five weeks. Store the past five grocery totals as a list in an array.
+ Create an expression that will use the items in the array to calculate the average amount spent on groceries.
+
+ Please note: there should only be one given for this problem set, the array holding the five weekly totals.
+ You should be able to access the array using the array access notation discussed earlier in the course.
+
+ Givens
+    An array with five weekly grocery totals.
+
+ Result Variable:
+    Total amount spent on groceries.
+    Average weekly grocery spending.
+
+ Result to Print:
+    “You have spent a total of $X on groceries over 5 weeks. That is an average of $X per week”
+ */
