a016f5592a7a2d887c79d82aca5293494153e57b
fs-keri <kritenour@fullsail.edu>
2015-06-09 23:07:20 -0400

    Calculate Dog Years
    Created variable and results for calculating the given number of human
    years for a dog resulting in the age of a dog in dog years.

--- Expressions_Worksheet/Dog Years/js/script.js
+++ Expressions_Worksheet/Dog Years/js/script.js
@@ -5,4 +5,35 @@
  * Expressions Worksheet - Dog Years
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+/*
+ Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”
+ Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+ Givens:
+    Sparky’s age.
+
+ Result Variable:
+    Sparky’s age in dog years.
+
+ Result to Print:
+    “Sparky is X human years old which is X in dog years.
+ */
+
+//Create variables for human years and dog years
+var humYears = 10;
+const dogYears = 7;
+
+//multiply humYears X dogYears to obtain result
+var result = humYears * dogYears;
+
+//print to screen
+console.log("Sparky is " +humYears+ " human years old which is " +result+ " in dog years.");
+
+/*
+    Typed in 7 for human years and got 49 as a result
+    Typed in 3 for human years and got 21 as a result
+    Typed in 10 for human years and got 70 as a result
+ */
+
