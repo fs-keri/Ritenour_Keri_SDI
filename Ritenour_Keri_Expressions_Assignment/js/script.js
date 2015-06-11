8f93d99eeb14ad0f6620c54f6123b5da81b774e7
fs-keri <kritenour@fullsail.edu>
2015-06-10 23:53:34 -0400

    Variable Creation
    Created variables and user prompts for bag style, user prompt selection
    for bag style, user prompt for tax rate, an array for shipping options
    and user prompt for shipping selection. Tested

--- Ritenour_Keri_Expressions_Assignment/js/script.js
+++ Ritenour_Keri_Expressions_Assignment/js/script.js
@@ -7,6 +7,31 @@
 
 //alert("Testing to see if connected");
 
+//Online Ordering System alert to user
 alert("Welcome to LAPBAGS Online Ordering! \nTo place an order select OK.");
+
+//create array to hold the price of each bag style
+var bagStyle = [150, 175, 200, 225, 250];
+
+//prompt user to select the product number of the bag they want to purchase
+var userStyleSelection = prompt("Enter the product number corresponding to the bag style you would like to purchase: \nProduct #0  Go Bag $150 \nProduct #1  On-the-Go $175 \nProduct #2  Going $200 \nProduct #3  Gone $225 \nProduct #4  Long Gone $250");
+console.log(userStyleSelection);
+
+//create variable to store the user's State sales tax rate for purchases
+var stateRate = prompt("Please enter the sales tax for the State in which you reside");
+
+//divide user input by 100 to get the sales tax percentage;
+stateRate /= 100;
+console.log(stateRate);
+
+//create array to hold the options for shipping prices
+var shipOptions = [10.50, 35, 75];
+var userShipSelection = prompt("Please select a shipping option number: \n0 for Regular Delivery \n1 for 2-Day Delivery \n2 for Overnight Delivery");
+console.log(userShipSelection);
+
+alert("You have selected the " +bagStyle[Number(userStyleSelection)]+ " at a state sales tax rate of " +stateRate+ "% and the " +userShipSelection +".");
+
+
+
 
 
