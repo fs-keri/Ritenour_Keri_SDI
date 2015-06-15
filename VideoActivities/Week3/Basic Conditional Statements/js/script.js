44e06374108084b2840c743cc4dd22de68521a63
fs-keri <kritenour@fullsail.edu>
2015-06-14 23:18:21 -0400

    Week 3 Basic Conditional Statements Screencast
    Followed along and completed code using an if statement.

--- VideoActivities/Week3/Basic Conditional Statements/js/script.js
+++ VideoActivities/Week3/Basic Conditional Statements/js/script.js
@@ -2,7 +2,17 @@
  * Keri Ritenour
  * 6/14/15
  * Section 02
- * Week 3 - Basic Conditional Statements
+ * Week 3 - Conditional Logic
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+
+//if oldEnough is false then nothing prints out.
