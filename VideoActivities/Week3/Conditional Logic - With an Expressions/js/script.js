80387bc9ef1e2e124ebd4a79e049b50390dcb20e
fs-keri <kritenour@fullsail.edu>
2015-06-14 23:33:06 -0400

    Week 2 Conditional Logic with an Expression
    Created and followed along to week 2 conditional logic with an
    expression.

---
+++ VideoActivities/Week3/Conditional Logic - With an Expressions/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Keri Ritenour
+ * 6/14/15
+ * Section 02
+ * Week 3 - Conditional Logic - With an Expression
+ */
+
+//alert("Testing to see if connected");
+
+var kidHeight = 50;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift> minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
