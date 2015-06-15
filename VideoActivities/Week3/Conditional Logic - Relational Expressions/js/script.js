bbefaf9d0584312cdaa5c8a493e83da6a294c23d
fs-keri <kritenour@fullsail.edu>
2015-06-14 23:27:59 -0400

    Week 3 Conditional Statements and Relational Expressions
    Created and followed along with week 3 screencast on conditional
    statements and relational expressions.

--- VideoActivities/Week3/Conditional Logic - Relational Expressions/js/script.js
+++ VideoActivities/Week3/Conditional Logic - Relational Expressions/js/script.js
@@ -5,4 +5,14 @@
  * Week 3 - Conditional Logic - Relational Expressions
  */
 
-alert("Testing to see if connected");
+//alert("Testing to see if connected");
+
+var kidHeight = 50;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
