df780b22ee5d3f270a58bfacf927507743c8746d
fs-keri <kritenour@fullsail.edu>
2015-06-16 09:12:39 -0400

    Finished with notes

--- VideoActivities/Week3/Conditional Logic - Logical Operators/js/script.js
+++ VideoActivities/Week3/Conditional Logic - Logical Operators/js/script.js
@@ -22,13 +22,34 @@
 //Logical operators are only put between boolean variables or between comparisons
   //something that is already being used with relational expressions
 
+// AND Operator
 //if the price of the phone is less our budget AND if your paycheck is over 300
 var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 
-if(iPhonePrice < budget && paycheck > 300){ //&& requires both are true
+if(iPhonePrice < budget && paycheck > 300){ // && requires both are true
    console.log("We can buy the phone!")
 }else{
     console.log("No phone for you!")
 }
+
+// OR Operator
+var budget = 300;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+if(iPhonePrice < budget || wonLottery === true){ // || only one needs to be true
+    console.log("We can buy the phone!")
+}else{
+    console.log("No phone for you!")
+}
+
+// ! NOT Operator - (flips the meaning of the object also called negation)
+
+//will not work a === b === c
+//should be written a === b && b === c
+
+// Relational operators go in between PAIRS of objects
+// Logical operators go in between Relational Expressions or Boolean values
+// Always work in Pairs (if working with 3 object break them down into pairs
