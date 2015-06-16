06dfc254cba7f9a00542c5a851b5e20062d13594
fs-keri <kritenour@fullsail.edu>
2015-06-16 08:51:50 -0400

    Week 3 Relational Operators Screencast
    Created js and html files and tested connection. Added notes for
    relational operators as comments.

---
+++ VideoActivities/Week3/Conditional Logic - Logical Operators/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Keri Ritenour
+ * 6/16/15
+ * Section 02
+ * Week 3 - Conditional Logic - Logical Operators
+ */
+
+//alert("Testing to see if connected");
+
+//Equality == compares two "is this the same"  a==b
+
+//Assignment = stores values  a=b "changes the value and stores it"
+
+//Strict Equality === "the same value and the same type"
+    // 6 == "6" TRUE
+    // 6 === "6"  FALSE (one is a number and one is a string)
+
+    //Use Strict Equality over Equality === is better and faster than ==
+
+//Inequality != "is not the same as"
