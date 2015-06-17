4951ab6a73e7559ae5b9a16dd232bbb3c8032e1e
fs-keri <kritenour@fullsail.edu>
2015-06-16 22:36:11 -0400

    Finished Chapter Examples

--- VideoActivities/Week3/Chapter Examples/js/script.js
+++ VideoActivities/Week3/Chapter Examples/js/script.js
@@ -7,7 +7,7 @@
 
 //alert("Testing to see if connected");
 
-var degFahren = Number(prompt("Enter the degrees Farenheit"));
+/*var degFahren = Number(prompt("Enter the degrees Farenheit"));
 var degCent;
 
 degCent = 5/9 * (degFahren -32);
@@ -19,3 +19,19 @@ if(degCent < 0){
 if(degCent == 100){
     console.log("That's the boiling point of water");
 }
+*/
+
+var myAge = Number(prompt("Enter your age"));
+
+if(myAge >=0 && myAge <= 10){
+    console.log("myAge is between 0 and 10");
+}
+    if(!(myAge >= 0 && myAge <= 10)){
+        console.log("myAge is NOT between 0 and 10");
+    }
+        if(myAge >= 80 || myAge <= 10){
+            console.log("myAge is 80 or above or 10 or below");
+        }
+            if((myAge >= 30 && myAge <= 39) || (myAge >= 80 && myAge <= 89)){
+                console.log("myAge is between 30 and 39 or myAge is between 80 and 89");
+            }
