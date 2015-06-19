3e5744c6624410068df585fffc09ac4f99148837
fs-keri <kritenour@fullsail.edu>
2015-06-18 20:10:25 -0400

    Variable for handbags
    Created variable for handbags and prompt user for selection

--- Ritenour_Keri_Conditionals_Assignment/js/script.js
+++ Ritenour_Keri_Conditionals_Assignment/js/script.js
@@ -6,3 +6,15 @@
  */
 
 //alert("Testing to see if connected");
+
+//alert user welcoming them to the online ordering system
+alert("Welcome to 550 Online Ordering! \nThe Home of 5 Hand Bags under $50 \nTo Place an order, select OK.");
+
+//create variable to store the price of the bag
+var bagPrice = [15.50, 20, 25, 35.50, 49.99];
+
+//prompt user for bag selection
+var userBagSelection = prompt("Enter the product number corresponding to the Hand Bag you would like to purchase: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
+//store selection
+console.log(userBagSelection);
+
