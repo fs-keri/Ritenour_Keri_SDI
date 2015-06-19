fb22eeb8097bd5805601c23034e4485e9f7b743d
fs-keri <kritenour@fullsail.edu>
2015-06-18 21:00:34 -0400

    Corrected Prompt Validation
    Corrected the prompt validation so that it checks twice.

--- Ritenour_Keri_Conditionals_Assignment/js/script.js
+++ Ritenour_Keri_Conditionals_Assignment/js/script.js
@@ -19,8 +19,8 @@ var userBagSelection = prompt("Enter the product number corresponding to the Han
 //create conditional to validate user input if blank or not a number
 if(userBagSelection === "" || isNaN(userBagSelection)){
     userBagSelection = prompt("Please do not leave blank. \nEnter the product number for the bag you would like to purchase: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
-} else{
-    userBagSelection;
+}if(userBagSelection === "" || isNaN(userBagSelection)){
+    userBagSelection = prompt("Only product numbers are valid. \nPlease enter the product number corresponding to the bag you are purchasing: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
 }
 //store selection
 console.log(userBagSelection);
