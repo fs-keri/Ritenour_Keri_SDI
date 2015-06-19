467dea4b482a3e7b71163072d8ebc8d75660e99d
fs-keri <kritenour@fullsail.edu>
2015-06-18 20:37:31 -0400

    Corrected code and tested

--- Ritenour_Keri_Conditionals_Assignment/js/script.js
+++ Ritenour_Keri_Conditionals_Assignment/js/script.js
@@ -19,10 +19,7 @@ var userBagSelection = prompt("Enter the product number corresponding to the Han
 //create conditional to validate user input if blank or not a number
 if(userBagSelection === "" || isNaN(userBagSelection)){
     userBagSelection = prompt("Please do not leave blank. \nEnter the product number for the bag you would like to purchase: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
-//validate input again if user leaves blank or does not enter a number
-}else if(isNaN(userBagSelection)){
-    userBagSelection = prompt("Only product numbers are valid. \nPlease enter the product number corresponding to the bag you are purchasing: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
-}else{
+} else{
     userBagSelection;
 }
 //store selection
