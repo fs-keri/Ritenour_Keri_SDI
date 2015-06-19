3813c1a0761d0d5f0e46690ede0850cf31a71031
fs-keri <kritenour@fullsail.edu>
2015-06-18 20:34:53 -0400

    Conditional to validate user prompt
    created conditional to validate user prompt for handbag style.

--- Ritenour_Keri_Conditionals_Assignment/js/script.js
+++ Ritenour_Keri_Conditionals_Assignment/js/script.js
@@ -15,6 +15,18 @@ var bagPrice = [15.50, 20, 25, 35.50, 49.99];
 
 //prompt user for bag selection
 var userBagSelection = prompt("Enter the product number corresponding to the Hand Bag you would like to purchase: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
+
+//create conditional to validate user input if blank or not a number
+if(userBagSelection === "" || isNaN(userBagSelection)){
+    userBagSelection = prompt("Please do not leave blank. \nEnter the product number for the bag you would like to purchase: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
+//validate input again if user leaves blank or does not enter a number
+}else if(isNaN(userBagSelection)){
+    userBagSelection = prompt("Only product numbers are valid. \nPlease enter the product number corresponding to the bag you are purchasing: \nProduct 0 Sweet Petite $15.50 \nProduct 1 Little Lucy $20.00 \nProduct 2 Regular Regina $25.00 \nProduct 3 Big Bertha $35.50 \nProduct 4 The Kitchen Sink $49.99");
+}else{
+    userBagSelection;
+}
 //store selection
 console.log(userBagSelection);
+
+
 
