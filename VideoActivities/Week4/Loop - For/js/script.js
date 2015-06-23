33c01c4bae7bbc70828a59792579fe0ad717335a
fs-keri <kritenour@fullsail.edu>
2015-06-23 09:03:10 -0400

    For Loops
    created js and html files for screencast on For Loops and followed
    along.

---
+++ VideoActivities/Week4/Loop - For/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Keri Ritenour
+ * 6/22/15
+ * Section 02
+ * Week 4 - Loops - For
+ */
+
+//alert("Testing to see if connected");
+
+//FOR Loop is a shorthand version
+
+for(var i = 10; i > 0; i--){
+    console.log(i + " kegs on the wall.");
+}
