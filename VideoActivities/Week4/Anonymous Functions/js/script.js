cbd2402058faeb81d9fb05f743c1db27228c863f
fs-keri <kritenour@fullsail.edu>
2015-06-23 00:21:36 -0400

    Anonymous Functions
    created js and html files for Anonymous Functions screencast and
    followed along.

---
+++ VideoActivities/Week4/Anonymous Functions/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Keri Ritenour
+ * 6/22/15
+ * Section 02
+ * Week 4 - Anonymous Functions
+ */
+
+//alert("Testing to see if connected");
+
+
+
+//defining the function must be done before calling
+var calcArea = function(width, height){
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30); invoking
+calcArea(a);
