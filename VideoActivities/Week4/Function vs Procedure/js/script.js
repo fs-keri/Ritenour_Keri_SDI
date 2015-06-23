adba26792aee24e5862b1479b6327bac4810d05b
fs-keri <kritenour@fullsail.edu>
2015-06-23 00:12:38 -0400

    Functions vs. Procedures
    created js and html files for Functions versus Procedures screencast
    and followed along.

---
+++ VideoActivities/Week4/Function vs Procedure/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Keri Ritenour
+ * 6/22/15
+ * Section 02
+ * Week 4 - Functions - Returning Values
+ */
+
+//alert("Testing to see if connected");
+
+//functions return values
+//procedures do not -- they print out
+
+//this is a function
+function calcArea(width, height){
+    var area = width * height;
+    return area; //function is spitting information out
+}//functions return values
+
+
+//this is a procedure
+function calcArea(width, height){
+    var area = width * height;
+    console.log(area)
+}//procedures do not -- they print out
+
+
+
