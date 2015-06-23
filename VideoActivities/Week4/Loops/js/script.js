7d4fab0fc264af0acfe570999aaa93fd231c1dbf
fs-keri <kritenour@fullsail.edu>
2015-06-23 08:52:32 -0400

    While Loop
    created js and html files for While Loop screencast and followed along.

---
+++ VideoActivities/Week4/Loops/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Keri Ritenour
+ * 6/22/15
+ * Section 02
+ * Week 4 - Loops
+ */
+
+//alert("Testing to see if connected");
+
+//WHILE Loop
+
+var b = 10; //this sets up the index
+
+while(b > 0){ //this block checks the condition
+    console.log(b + " kegs on the wall.");
+    b --; //increments (++) or decrements (--) the index
+} //not a good while loop because it is infinite
