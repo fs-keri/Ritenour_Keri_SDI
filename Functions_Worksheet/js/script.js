84c432e54fa2b5ee0d4a2d46c34cc0f80ddfa0a2
fs-keri <kritenour@fullsail.edu>
2015-06-23 22:15:45 -0400

    Calculate Area of a Rectangle
    added variable for width, height and area and function for calculation
    and printed to console.log.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -5,4 +5,15 @@
  * Functions Worksheet
  */
 
-alert("Testing to see if connected");
+//testing to see if connected
+//alert("Testing to see if connected");
+
+//calculate the area of a rectangle given the width and height of the rectangle
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log("The Area of the Rectangle is " +area);
+
+function calculateArea(w,h){
+    return w*h;
+}
