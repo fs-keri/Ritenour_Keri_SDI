72739180fa9e23d8f7b38d6f1290f4319e5be6e3
fs-keri <kritenour@fullsail.edu>
2015-06-25 19:07:06 -0400

    Variables Created
    user prompts variable created for check total, tip amount and service
    rating.

--- Ritenour_Keri_Functions_Assignment/js/script.js
+++ Ritenour_Keri_Functions_Assignment/js/script.js
@@ -10,4 +10,11 @@
 
 //Calculate the tip you would leave at a restaurant based on the total amount purchased and the service rating
 
-//
+//create variables
+var checkTotal = prompt("Enter the total of your check rounded up to the next whole dollar before taxes:");
+var percentTip = prompt("Enter the percentage amount you would like to tip:");
+var serviceRating = prompt("On a scale of 1 to 5, how would you rate this restaurant? \n1 = Poor \n2 = Fair \n3 = Mediocre \n4 = Good \n5 = Great!");
+
+
+
+var totalWithTip = tipTotal(checkTotal, percentTip, serviceRating);
